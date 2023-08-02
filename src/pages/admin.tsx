0d9c5aec570ad65d182d7b33d46217390717359e
CandiceZFish <daintycandy@gmail.com>
2023-08-02 16:20:16 -0700

    simple ui in CN

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -2,18 +2,24 @@ import React from "react";
 
 export default function admin() {
   return (
-    <div className="container">
-      <div>
-        <h1>Admin page</h1>
+    <main className="d-flex h-100 text-center text-bg-dark">
+      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column m-5">
+        <div>
+          <h1 className="title">管理员 Admin page</h1>
+        </div>
+        <div>
+          <p>Current Link 现有链接: ... </p>
+          <p> Created time 创建时间: ...</p>
+        </div>
+        <div>
+          <input type="text" placeholder="Enter a new link" />
+          <button className="btn btn-primary m-2">Update 更新</button>
+        </div>
+        <div className="mt-5">
+          <p>List of history links 历史链接:</p>
+          <p>... | 创建时间 | 失效时间</p>
+        </div>
       </div>
-      <div>
-        <p>Current Link: ... </p>
-        <p> Created time: ...</p>
-      </div>
-      <div>
-        <input type="text" placeholder="Enter a new link" />
-        <button className="btn btn-primary m-2">Update</button>
-      </div>
-    </div>
+    </main>
   );
 }
